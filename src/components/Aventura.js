653bd651499dd220d6323033a743ab923d778045
Nick07242000 <nicohidalgo2000@gmail.com>
2022-04-03 22:20:04 -0300

    Added Lifecycle in Aventura

--- src/components/Aventura.js
+++ src/components/Aventura.js
@@ -8,29 +8,30 @@ export class Aventura extends Component {
     this.state = {
       contador : 1,
       historial : [],
-      seleccionPrevia : ""
+      seleccionPrevia : "",
+      historiaActual : {}
     };
   }
 
-  historiaActual = () => {
-    let h = this.state.contador.toString() + this.state.seleccionPrevia.toLowerCase();
-    return this.props.historia.find((historia) => historia.id === h)
+  static getDerivedStateFromProps(props, state) {
+    return { historiaActual : props.historia.find((historia) => historia.id === state.contador.toString() + state.seleccionPrevia.toLowerCase())}
   }
 
   actualizar = (letter) => {
-    if(this.historiaActual().opciones.a === "FIN.") {alert("Fin."); return}
+    if((this.props.historia.length - 1)/2 < this.state.contador) {alert("Fin."); return}
     this.setState({
       contador : this.state.contador + 1,
       historial : (this.state.contador !== 1) ? [...this.state.historial, letter] : [],
       seleccionPrevia : letter
     })
-  }
+  } 
 
   render() {
+    //let historiaActual = this.props.historia.find((historia) => historia.id === (this.state.contador.toString() + this.state.seleccionPrevia.toLowerCase()));
     return (
       <div className="layout">
-        <h1 className="aventura">{this.historiaActual().historia}</h1>
-        <Opciones opciones={this.historiaActual().opciones} evento={this.actualizar}/>
+        <h1 className="aventura">{this.state.historiaActual.historia}</h1>
+        <Opciones opciones={this.state.historiaActual.opciones} evento={this.actualizar}/>
         <Historial historial={this.state.historial} seleccionPrevia={this.state.seleccionPrevia}/>
       </div>
     );
